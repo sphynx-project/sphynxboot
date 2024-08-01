66941e724a8dc8de9b1207059c1d18278607dd80
Kevin Alavik <kevin@alavik.se>
2024-08-01 21:51:25 +0200

    new cat

--- common/src/main.c
+++ common/src/main.c
@@ -1,16 +1,10 @@
-/*
-sphynxboot - A bootloader for modern systems.
-Written in 2024 by Kevin Alavik <kevin@alavik.se>
-
-To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
-*/
-
 #include <common.h>
 #include <fs/sfs.h>
 #include <lib/print.h>
 #include <lib/alloc.h>
 #include <lib/string.h>
 #include <boot/kernel.h>
+#include <efi.h>
 
 EFI_HANDLE *imageHandle;
 EFI_SYSTEM_TABLE *systemTable;
@@ -19,15 +13,40 @@ EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *stdout;
 EFI_SIMPLE_TEXT_INPUT_PROTOCOL *stdin;
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *stderr;
 
-char *logo[] = {
-    "|\\---/|",
-    "| o_o |  Sphynx Bootloader v0.0.1",
-    " \\_^_/"
-};
+void delay_seconds(int seconds) {
+    EFI_STATUS status;
+    EFI_UINT64 triggerTime = seconds * 10000000ULL;
+    EFI_EVENT timerEvent;
+    
+    systemTable->BootServices->CreateEvent(
+        EVT_TIMER,
+        TPL_CALLBACK,
+        NULL,
+        NULL,
+        &timerEvent
+    );
+    
+    status = systemTable->BootServices->SetTimer(
+        timerEvent,
+        TimerRelative,
+        triggerTime
+    );
+    if (EFI_ERROR(status)) {
+        printf("Error setting timer.\n");
+        systemTable->BootServices->CloseEvent(timerEvent);
+        return;
+    }
 
+    EFI_UINTN index;
+    status = systemTable->BootServices->WaitForEvent(1, &timerEvent, &index);
+    if (EFI_ERROR(status)) {
+        printf("Error waiting for event.\n");
+    }
 
-EFI_STATUS
-sphynxboot_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+    systemTable->BootServices->CloseEvent(timerEvent);
+}
+
+EFI_STATUS sphynxboot_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     imageHandle = &ImageHandle;
     systemTable = &*SystemTable;
@@ -39,9 +58,16 @@ sphynxboot_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     stdout->SetCursorPosition(stdout, 0, 0);
     stdout->ClearScreen(stdout);
 
+    char *logo[] = { 
+        " _._     _,-'\"\"`-._",
+        "(,-.`._,'(       |\\`-/|   Sphynxboot v0.0.1",
+        "    `-.-' \\ )-`( , o o)",
+        "          `-    \\`_`\"'-"
+    };
+
     for (int i = 0; i < sizeof(logo) / sizeof(logo[0]); i++) {
         for(int j = 0; j < strlen(logo[i]); j++) {
-            if (i == 1 && j >= 9) {
+            if (i == 1 && j >= 26) {
                 stdout->SetAttribute(stdout, EFI_CYAN | EFI_BACKGROUND_BLACK);
             } else {
                 stdout->SetAttribute(stdout, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
@@ -51,8 +77,21 @@ sphynxboot_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
         printf("\n");
     }
 
+    printf("\n\n");
+
+    int countdown = 3;
+    printf("Booting in 3...");
+    while (countdown > 1) {
+        stdout->SetAttribute(stdout, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
+        delay_seconds(1);
+        countdown--;
+        printf("%d...", countdown);
+    }
+
+    delay_seconds(1);
+    printf("0\n");
+
     load_kernel();
 
-    for (;;)
-        ;
+    return EFI_SUCCESS;
 }
